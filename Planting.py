15a172e4af82b8a0d004ddd149275a6f94673877
coencoensmeets <coencoensmeets@gmail.com>
2025-01-09 19:59:45 +0100

    Refactor planting functions to support intercropping of trees, carrots, and wheat

--- Planting.py
+++ Planting.py
@@ -1,47 +1,31 @@
-def plant_bushes(N):	
+def plant_tree():
+	if can_harvest() or get_entity_type() == None:
+		harvest()
+		plant(Entities.Tree)
+
+def plant_carrot():
+	if can_harvest() or get_entity_type() == None:
+		harvest()
+		if (get_ground_type() != Grounds.Soil):
+			till()
+		trade(Items.Carrot_Seed)
+		plant(Entities.Carrots)
+
+def plant_wheat():
+	if can_harvest() or get_entity_type() == None:
+		harvest()
+		plant(Entities.Grass)
+
+def intercropping():
 	clear()
 	world_size = get_world_size()
-	for i in range(N):
+	while True:
 		for n in range(world_size):
-			if can_harvest():
-				harvest()
-				plant(Entities.Bush)
+			if get_pos_x() % 2 == 0 and get_pos_y() % 2 == 0:
+				plant_tree()
+			elif get_pos_y() < world_size // 2:
+				plant_carrot()
+			else:
+				plant_wheat()
 			move(East)
 		move(North)
-
-def plant_carrots(N):
-	clear()
-	world_size = get_world_size()
-	for i in range(N):
-		for n in range(world_size):
-			if can_harvest():
-				harvest()
-				if get_ground_type() != Grounds.Soil:
-					till()
-				trade(Items.Carrot_Seed)
-				plant(Entities.Carrots)
-			move(East)
-		move(North)
-
-def plant_wheat(N):
-	clear()
-	world_size = get_world_size()
-	for i in range(N):
-		for n in range(world_size):
-			if can_harvest():
-				harvest()
-				plant(Entities.Grass)
-			move(East)
-		move(North)
-
-def crop_to_plant():
-	N_wheat = num_items(Items.Hay)
-	N_carrot = num_items(Items.Carrot)
-	N_bush = num_items(Items.Wood)
-
-	if N_wheat < N_carrot and N_wheat < N_bush:
-		return Entities.Grass
-	elif N_carrot < N_wheat and N_carrot < N_bush:
-		return Entities.Carrots
-	else:
-		return Entities.Bush
--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from Planting import plant_bushes, plant_carrots, plant_wheat, crop_to_plant
+from Planting import intercropping
 
 while True:
-	N=20
-	
-	crop = crop_to_plant()
-	
-	if crop == Entities.Grass:
-		plant_wheat(N)
-	elif crop == Entities.Carrots:
-		plant_carrots(N)
-	else:
-		plant_bushes(N)
+	intercropping()
